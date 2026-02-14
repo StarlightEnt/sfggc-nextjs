207a6e327b5fedcc51e712b6bac9ab5e305d0101
AllisonDL <allisushi@gmail.com>
2026-02-14 03:34:54 -0800

    Update ScrMstWinners.js

--- src/components/ScrMstWinners/ScrMstWinners.js
+++ src/components/ScrMstWinners/ScrMstWinners.js
@@ -21,74 +21,73 @@ const ScrMstWinners = () => {
       </div>
 
        <div className={`row justify-content-center`}>
-      //   <div className={`col-6 col-sm-4 col-lg-3`}>
-      //     <div className={`card ${styles.Card}`}>
-      //       <Image src={a_winner}
-      //              alt={'Daniel Chin'}
-      //              className={`img-fluid card-img-top ${styles.Image}`}
-      //       />
-      //       <div className={`card-body`}>
-      //         <p className={`${styles.Name}`}>
-      //             Daniel Chin
-      //         </p>
-      //         <p className={styles.Role}>
-      //           Division A Winner
-      //         </p>
-      //       </div>
-      //     </div>
+         <div className={`col-6 col-sm-4 col-lg-3`}>
+           <div className={`card ${styles.Card}`}>
+             <Image src={a_winner}
+                    alt={'Daniel Chin'}
+                    className={`img-fluid card-img-top ${styles.Image}`}
+             />
+             <div className={`card-body`}>
+               <p className={`${styles.Name}`}>
+                   Daniel Chin
+               </p>
+               <p className={styles.Role}>
+                 Division A Winner
+               </p>
+             </div>
+           </div>
+         </div>
 
-      //   </div>
+         <div className={`col-6 col-sm-4 col-lg-3`}>
+           <div className={`card ${styles.Card}`}>
+             <Image src={b_winner}
+                    alt={'Russ Thatcher'}
+                    className={`img-fluid card-img-top ${styles.Image}`}
+             />
+             <div className={`card-body`}>
+               <p className={`${styles.Name}`}>
+                   Russ Thatcher
+                </p>
+               <p className={styles.Role}>
+                 Division B Winner
+               </p>
+             </div>
+           </div>
+         </div>
 
-      //   <div className={`col-6 col-sm-4 col-lg-3`}>
-      //     <div className={`card ${styles.Card}`}>
-      //       <Image src={b_winner}
-      //              alt={'Russ Thatcher'}
-      //              className={`img-fluid card-img-top ${styles.Image}`}
-      //       />
-      //       <div className={`card-body`}>
-      //         <p className={`${styles.Name}`}>
-      //             Russ Thatcher
-      //          </p>
-      //         <p className={styles.Role}>
-      //           Division B Winner
-      //         </p>
-      //       </div>
-      //     </div>
-      //   </div>
+         <div className={`col-6 col-sm-4 col-lg-3`}>
+           <div className={`card ${styles.Card}`}>
+             <Image src={c_winner}
+                    alt={'Robert Michaels'}
+                    className={`img-fluid card-img-top ${styles.Image}`}
+             />
+             <div className={`card-body`}>
+               <p className={`${styles.Name}`}>
+                   Robert Michaels
+               </p>
+               <p className={styles.Role}>
+                 Division C Winner
+               </p>
+             </div>
+           </div>
+         </div>
 
-      //   <div className={`col-6 col-sm-4 col-lg-3`}>
-      //     <div className={`card ${styles.Card}`}>
-      //       <Image src={c_winner}
-      //              alt={'Robert Michaels'}
-      //              className={`img-fluid card-img-top ${styles.Image}`}
-      //       />
-      //       <div className={`card-body`}>
-      //         <p className={`${styles.Name}`}>
-      //             Robert Michaels
-      //         </p>
-      //         <p className={styles.Role}>
-      //           Division C Winner
-      //         </p>
-      //       </div>
-      //     </div>
-      //   </div>
-
-      //   <div className={`col-6 col-sm-4 col-lg-3`}>
-      //     <div className={`card ${styles.Card}`}>
-      //       <Image src={d_winner}
-      //              alt={'Jason Eller'}
-      //              className={`img-fluid card-img-top ${styles.Image}`}
-      //       />
-      //       <div className={`card-body`}>
-      //         <p className={`${styles.Name}`}>
-      //             Jason Eller
-      //         </p>
-      //         <p className={styles.Role}>
-      //           Division D Winner
-      //         </p>
-      //       </div>
-      //     </div>
-      //   </div>
+         <div className={`col-6 col-sm-4 col-lg-3`}>
+           <div className={`card ${styles.Card}`}>
+             <Image src={d_winner}
+                    alt={'Jason Eller'}
+                    className={`img-fluid card-img-top ${styles.Image}`}
+             />
+             <div className={`card-body`}>
+               <p className={`${styles.Name}`}>
+                   Jason Eller
+               </p>
+               <p className={styles.Role}>
+                 Division D Winner
+               </p>
+             </div>
+           </div>
+         </div>
       </div>
 
     </section>
@@ -96,5 +95,6 @@ const ScrMstWinners = () => {
 }
 
 export default ScrMstWinners;
+
 
 
